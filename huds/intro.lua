5634755b864c7bbde9ecadb32582b8558220091a
Ashley Pringle <ash.pringle@gmail.com>
2017-03-28 00:07:32 -0300

    lmost

--- huds/intro.lua
+++ huds/intro.lua
@@ -1,11 +1,6 @@
 local function make(i)
 	i.canvas=LG.newCanvas(640,480)
-	local top_left  = {200, 0,   0,     0, 255, 255, 255, 255} 
-	local top_right = {400, 0,   1,     0, 255, 255, 255, 255}
-	local bot_right = {640, 480, 1,     1, 255, 255, 255, 255}
-	local bot_left  = {0,   480, 0,     1, 255, 255, 255, 255}
-	local vertexes = {top_left,top_right,bot_right,bot_left}
-	i.mesh = love.graphics.newMesh(vertexes, "fan", "dynamic")
+
 end
 
 local function control()
@@ -29,12 +24,31 @@ local function draw(i)
 		LG.clear()
 		--LG.setColor(Palette[EC.blue])
 		--LG.rectangle("fill",0,0,640,480)
-		LG.print("INTRO\nBLAHBLAHBLAH",Game.width/2,400-Game.timer)
-
+		LG.setColor(Palette[EC.white])
+		LG.print("INTRO\nBLAHBLAHBLAH",Game.width/2,Game.height-Game.timer)
 	LG.setCanvas()
-	--love.graphics.draw(i.canvas,Screen.xoff,Screen.yoff,0,Screen.scale,Screen.scale,0,0,-0.5,0)
-	i.mesh:setTexture(i.canvas)
-	love.graphics.draw(i.mesh,0,0)
+
+	local cw,ch=i.canvas:getWidth(),i.canvas:getHeight()
+	local imgdata=i.canvas:newImageData(0,0,cw-1,ch-1)
+	local iw,ih=imgdata:getWidth(),imgdata:getHeight()
+	local mid=iw/2
+	
+	for x = iw-1,0,-1 do
+		local xoff=math.floor(x-mid)
+	    for y = ih-1,0,-1  do
+			local ynorm=y/ih
+			local xoffsquish=xoff*ynorm
+			local xsquish=math.clamp(xoffsquish+mid,0,iw)
+			local r,g,b,a = imgdata:getPixel(x,y)
+			--imgdata:setPixel(math.min(x+10,imgdata:getWidth()-1),y,r,g,b,a)
+			--imgdata:setPixel(math.min(xsquish,iw-1),y,r,g,b,a)
+			--imgdata:setPixel(xsquish,imgdata:getWidth()-1,y,r,g,b,a)
+			imgdata:setPixel(xsquish,y,r,g,b,a)
+	    end
+	end
+	local image=LG.newImage(imgdata)
+	--love.graphics.draw(i.canvas,Screen.xoff,Screen.yoff,0,Screen.scale,Screen.scale,0,0,0,0)
+	love.graphics.draw(image,Screen.xoff,Screen.yoff,0,Screen.scale,Screen.scale,0,0,0,0)
 end
 
 return
