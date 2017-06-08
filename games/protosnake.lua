8ede184b37f1898eae006711dff4a345bc6739b1
Ashley Pringle <ash.pringle@gmail.com>
2017-06-07 21:36:05 -0300

    test

--- games/protosnake.lua
+++ games/protosnake.lua
@@ -2,7 +2,6 @@ local function make(g,tw,th,gw,gh,sp)
 	g.pt=Enums.physics.topdown
 	g.levels=level.load("levels/protosnake/inis")
 	g.levelpath={}
-	--debugger.printtable(g.levels)
 
 	game.state.make(g,Enums.states.intro)
 end
