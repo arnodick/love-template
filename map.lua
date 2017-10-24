b019f826a0155c2a74b89e3ecd03e3efcb5403dc
Ashley Pringle <ash.pringle@gmail.com>
2017-10-24 18:24:20 -0300

    another bites ma butt

--- map.lua
+++ map.lua
@@ -113,7 +113,7 @@ drawmodes.isometric = function(m)
 			--if (y-1)*#m[y]+x<=t then
 				local isox=(x-1)*tw/2
 				local isoy=(y-1)*th/4
-				local value=m[y][x]
+				local value=bit.band(m[y][x],65535)
 				--LG.draw(Spritesheet[3],Quads[3][value],isox+230,isoy+50,0,1,1,(y-1)*tw/2,(x-1)*-th/4)
 				LG.draw(Spritesheet[3],Quads[3][value],isox,isoy,0,1,1,(y-1)*tw/2,(x-1)*-th/4)
 				if Debugger.debugging then
