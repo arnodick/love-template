582930c008d34cdff7162302ca99ddb10a63d184
Ashley Pringle <ash.pringle@gmail.com>
2018-05-21 18:14:32 -0300

    updated supper

--- game.lua
+++ game.lua
@@ -91,15 +91,7 @@ end
 
 game.control = function(g)
 	hud.control(g,g.hud)
---[[
-	if g.hud then
-		if g.hud.menu then
-			--g.hud.menu.x=g.camera.x
-			--g.hud.menu.y=g.camera.y
-			menu.control(g.hud.menu,g.speed)
-		end
-	end
---]]
+
 	game.state.run(g.name,g.state,"control",g)
 
 	sfx.update(SFX,g.speed)
