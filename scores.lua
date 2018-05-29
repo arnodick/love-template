9e70452b6947cfa9182429743ef6268b5787cad6
Ashley Pringle <ash.pringle@gmail.com>
2018-05-29 20:51:22 -0300

    going to update hi-scores

--- scores.lua
+++ scores.lua
@@ -2,14 +2,15 @@ local function load()
 	if not love.filesystem.exists("scores.ini") then
 		local h={}
 		local n={}
-		for i=1,8 do
-			table.insert(h,0)
-			table.insert(n,"ABC")
+		for i=1,5 do
+			table.insert(h,i*5)
+			table.insert(n,"ASH")
 		end
 		local scores=
 		{
 			high=h,
-			names=n
+			names=n,
+			--saved=false
 		}
 		
 		LIP.save("scores.ini",scores)
@@ -17,14 +18,14 @@ local function load()
 	return LIP.load("scores.ini")
 end
 
-local function save()
+local function update()
 	local scores=scores.load()
 	local s={}
 	for j=1,#scores.high do
 		table.insert(s,{scores.names[j],scores.high[j]})
 	end
 	local score=Game.score
-	table.insert(s,{"ASH",score})
+	table.insert(s,{"",score})
 
 	local function scoresort(a,b)
 		if a[2]>b[2] then
@@ -35,8 +36,9 @@ local function save()
 	end
 
 	table.sort(s,scoresort)
+
 	for i=#s,1,-1 do
-		if i>8 then
+		if i>5 then
 			table.remove(s,i)
 		end
 	end
@@ -49,11 +51,21 @@ local function save()
 	end
 
 	Game.scores=scores
-	LIP.save("scores.ini",scores)
+end
+
+local function save()
+	for i=1,#Game.scores.names do
+		if Game.scores.names[i]=="" then
+			Game.scores.names[i]="X"
+		end
+	end
+	LIP.save("scores.ini",Game.scores)
+	Game.scores.saved=true
 end
 
 return
 {
 	load = load,
+	update = update,
 	save = save,
 }
