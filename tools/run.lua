da2f45397ef47e530aebdc0df8fdf735528eb247
Ashley Pringle <ash.pringle@gmail.com>
2019-05-18 23:55:01 -0300

    i don't know why WE were returning true from run?

--- tools/run.lua
+++ tools/run.lua
@@ -2,7 +2,7 @@ local run = function(libraryname, functionname,...)
 	--runs a function in a library, after checking to make sure it exists
 	if _G[libraryname][functionname] then
 		_G[libraryname][functionname](...)
-		return true
+		-- return _G[libraryname][functionname](...)--is there any point in doing this?
 	end
 end
 
