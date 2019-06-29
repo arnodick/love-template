a341f0e1f5c2761017fd5f77a7f4018814549217
Ashley Pringle <ash.pringle@gmail.com>
2019-06-28 23:56:37 -0300

    first colour converted!

--- modules/menus/text.lua
+++ modules/menus/text.lua
@@ -3,10 +3,10 @@ text={}
 text.draw = function(m)
 	if type(m.text)=="table" then
 		for i=1,#m.text do
-			local linealpha=255
+			local linealpha=1
 			if m.text.index then
 				if m.text.index~=i then
-					linealpha=50
+					linealpha=0.2--TODO have transparency value in menu? default to 0.2
 				end
 			end
 			LG.printformat(m.text[i],m.x-m.w/2,m.y-m.h/2+12*i,m.w,m.align,m.c1,m.c2,linealpha)
