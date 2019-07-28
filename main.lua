607ad54f3cf1c83f8bfa748b640ea63c4876fd8b
Ashley Pringle <ash.pringle@gmail.com>
2019-07-28 11:27:55 -0300

    Broken?

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 --game initialization stuff (just boring stuff you need to maek Video Game)
 library=require("tools/library")--have to load the library.lua library to use it to dynamically load the rest of the libraries
 library.load("","lua")--loads all the .lua libraries
+-- supper.print(l,"LIBRARY TABLE")
 
 Enums=enums.load("","games","actors","modules","modes")--enumerators
 enums.constants(Enums)--constants derived from enums, they're shorthand so you can type EM instead of Enums.modules
