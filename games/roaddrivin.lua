63ac42cc4569d96d83ddea8b548051d9b037f2c8
Ashley Pringle <ash.pringle@gmail.com>
2020-11-24 01:26:45 -0400

    new files

---
+++ games/roaddrivin.lua
@@ -0,0 +1,99 @@
+local roaddrivin={}
+
+roaddrivin.make = function(g)
+
+end
+
+roaddrivin.level={}
+
+roaddrivin.gameplay =
+{
+	make = function(g)
+		--TODO just do this, take map generators, args, drawmodes out of level json file
+		--map.generate(g.map,"country",{arguyments here})
+		level.make(g,1,Enums.modes.topdown)
+		-- local m=g.level.map
+		-- actor.make(g,EA.roaddrivin_actor,m.width/2-5,m.height/2-5)
+	end,
+
+	keypressed = function(g,key)
+		local ease=easing.outElastic
+		local dist=300
+		local d=300
+		if key=='escape' then
+			game.state.make(g,"title")
+		elseif key=='z' then
+			module.make(g,g.camera,EM.transition,ease,"zoom",g.camera.zoom,4,d)
+		elseif key=='x' then
+			module.make(g,g.camera,EM.transition,ease,"zoom",g.camera.zoom,-4,d)
+		end
+	end,
+
+	draw = function(g)
+		-- LG.print("GAEM_PLAY", g.width/2, g.height/2)
+	end
+}
+
+roaddrivin.title =
+{
+	keypressed = function(g,key)
+		if game.keyconfirm(key) then
+			game.state.make(g,"gameplay")
+		elseif key=='escape' then
+			game.state.make(g,"intro")
+		end
+	end,
+
+	gamepadpressed = function(g,joystick,button)
+		if button=="start" or button=="a" then
+			game.state.make(g,"gameplay")
+		end
+		if button=="b" then
+			game.state.make(g,"intro")
+		end
+	end,
+
+	draw = function(g)
+		LG.print("TITLE", g.width/2, g.height/2)
+	end
+}
+
+roaddrivin.intro =
+{
+	keypressed = function(g,key)
+		if game.keyconfirm(key) then
+			game.state.make(g,"title")
+		end
+	end,
+
+	gamepadpressed = function(g,joystick,button)
+		if button=="start" or button=="a" then
+			game.state.make(g,"title")
+		end
+	end,
+
+	draw = function(g)
+		LG.print("INTRO", g.width/2, g.height/2)
+	end
+}
+
+roaddrivin.option =
+{
+	keypressed = function(g,key)
+		if key=='escape' then
+			game.state.make(g,"title")
+		end
+	end,
+
+	gamepadpressed = function(g,joystick,button)
+		if button=="b" then
+			game.state.make(g,"intro")
+		end
+	end,
+
+	draw = function(g)
+		LG.print("OPTION",g.width/2,g.height/2)
+	end
+}
+
+return roaddrivin
